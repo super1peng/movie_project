18b3102141431dca4d6106721fd076bdba8ca2fb
super1peng <super1peng@gmail.com>
2018-09-08 22:34:13 +0800

    后台界面更新

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,7 +2,108 @@
 
 # 调用蓝图
 from . import admin
+from flask import render_template, redirect, url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style= 'color:red'>this is admin</h1>"
+    return render_template('admin/index.html')
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
+
+@admin.route("/movie/add/")
+def movie_add():
+    return render_template("admin/movie_add.html")
+
+@admin.route("/movie/list/")
+def movie_list():
+    return render_template("admin/movie_list.html")
+
+@admin.route("/preview/add/")
+def preview_add():
+    return render_template("admin/preview_add.html")
+
+@admin.route("/preview/list/")
+def preview_list():
+    return render_template("admin/preview_list.html")
+
+# 会员列表
+@admin.route("/user/list/")
+def user_list():
+    return render_template("admin/user_list.html")
+
+# 查看会员详情
+@admin.route("/user/view/")
+def user_view():
+    return render_template("admin/user_view.html")
+
+# 评论列表
+@admin.route("/comment/list/")
+def comment_list():
+    return render_template("admin/comment_list.html")
+
+# 收藏列表
+@admin.route("/moviecol/list/")
+def moviecol_list():
+    return render_template("admin/moviecol_list.html")
+
+
+# 日志管理
+# 操作日志
+@admin.route("/oplog/list/")
+def oplog_list():
+    return render_template("admin/oplog_list.html")
+
+# 登录日志
+@admin.route("/adminloginlog/list/")
+def adminloginlog_list():
+    return render_template("admin/adminloginlog_list.html")
+
+# 会员登录日志
+@admin.route("/userloginlog/list/")
+def userloginlog_list():
+    return render_template("admin/userloginlog_list.html")
+
+# 添加角色列表
+@admin.route("/role/add/")
+def role_add():
+    return render_template("admin/role_add.html")
+
+@admin.route("/role/list/")
+def role_list():
+    return render_template("admin/role_list.html")
+
+# 权限
+@admin.route("/auth/add/")
+def auth_add():
+    return render_template("admin/auth_add.html")
+
+@admin.route("/auth/list/")
+def auth_list():
+    return render_template("admin/auth_list.html")
+
+# 管理员
+@admin.route("/admin/add/")
+def admin_add():
+    return render_template("admin/admin_add.html")
+
+@admin.route("/admin/list/")
+def admin_list():
+    return render_template("admin/admin_list.html")
